6a74512cdc52b457bf115513a6400107a1d74689
bayu iman <senjuid@gmail.com>
2022-06-19 10:16:13 +0700

    addd

--- main.js
+++ main.js
@@ -1,42 +1,14 @@
 /******/ var __webpack_modules__ = ({
 
-/***/ 1195:
-/*!***********************!*\
-  !*** container entry ***!
-  \***********************/
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+/***/ 4431:
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-var moduleMap = {
-	"./MicroFrontEndModule": () => {
-		return __webpack_require__.e("src_app_microfrontend_module_ts-_06460").then(() => (() => ((__webpack_require__(/*! .//src/app/microfrontend.module.ts */ 9594)))));
-	}
-};
-var get = (module, getScope) => {
-	__webpack_require__.R = getScope;
-	getScope = (
-		__webpack_require__.o(moduleMap, module)
-			? moduleMap[module]()
-			: Promise.resolve().then(() => {
-				throw new Error('Module "' + module + '" does not exist in container.');
-			})
-	);
-	__webpack_require__.R = undefined;
-	return getScope;
-};
-var init = (shareScope, initScope) => {
-	if (!__webpack_require__.S) return;
-	var name = "default"
-	var oldScope = __webpack_require__.S[name];
-	if(oldScope && oldScope !== shareScope) throw new Error("Container initialization failed as it has already been initialized with a different share scope");
-	__webpack_require__.S[name] = shareScope;
-	return __webpack_require__.I(name, initScope);
-};
+__webpack_require__.e(/*! import() */ "src_bootstrap_ts").then(__webpack_require__.bind(__webpack_require__, /*! ./bootstrap */ 5533))
+    .catch(err => console.error(err));
 
-// This exports getters to disallow modifications
-__webpack_require__.d(exports, {
-	get: () => (get),
-	init: () => (init)
-});
 
 /***/ })
 
@@ -439,21 +411,22 @@ __webpack_require__.d(exports, {
 /******/ 	var installedModules = {};
 /******/ 	var moduleToHandlerMapping = {
 /******/ 		8230: () => (loadStrictSingletonVersionCheckFallback("default", "@angular/core", [2,13,3,0], () => (__webpack_require__.e("node_modules_angular_core_fesm2020_core_mjs").then(() => (() => (__webpack_require__(/*! @angular/core */ 2560))))))),
-/******/ 		4025: () => (loadStrictSingletonVersionCheckFallback("default", "@angular/common", [2,13,3,0], () => (__webpack_require__.e("node_modules_angular_common_fesm2020_common_mjs-_ec490").then(() => (() => (__webpack_require__(/*! @angular/common */ 4666))))))),
-/******/ 		8317: () => (loadStrictSingletonVersionCheckFallback("default", "@angular/router", [2,13,3,0], () => (__webpack_require__.e("node_modules_angular_router_fesm2020_router_mjs-_6f000").then(() => (() => (__webpack_require__(/*! @angular/router */ 124)))))))
+/******/ 		8317: () => (loadStrictSingletonVersionCheckFallback("default", "@angular/router", [2,13,3,0], () => (__webpack_require__.e("node_modules_angular_router_fesm2020_router_mjs-_6f000").then(() => (() => (__webpack_require__(/*! @angular/router */ 124))))))),
+/******/ 		4025: () => (loadStrictSingletonVersionCheckFallback("default", "@angular/common", [2,13,3,0], () => (__webpack_require__.e("node_modules_angular_common_fesm2020_common_mjs-_ec490").then(() => (() => (__webpack_require__(/*! @angular/common */ 4666)))))))
 /******/ 	};
 /******/ 	// no consumes in initial chunks
 /******/ 	var chunkMapping = {
+/******/ 		"src_bootstrap_ts": [
+/******/ 			8230,
+/******/ 			8317,
+/******/ 			4025
+/******/ 		],
 /******/ 		"node_modules_angular_common_fesm2020_common_mjs-_ec491": [
 /******/ 			8230
 /******/ 		],
 /******/ 		"node_modules_angular_router_fesm2020_router_mjs-_6f001": [
 /******/ 			4025,
 /******/ 			8230
-/******/ 		],
-/******/ 		"src_app_microfrontend_module_ts-_06460": [
-/******/ 			8230,
-/******/ 			8317
 /******/ 		],
 /******/ 		"node_modules_angular_router_fesm2020_router_mjs-_6f000": [
 /******/ 			4025
@@ -502,7 +475,7 @@ __webpack_require__.d(exports, {
 /******/ 	// undefined = chunk not loaded, null = chunk preloaded/prefetched
 /******/ 	// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 	var installedChunks = {
-/******/ 		"app": 0
+/******/ 		"main": 0
 /******/ 	};
 /******/ 	
 /******/ 	__webpack_require__.f.j = (chunkId, promises) => {
@@ -589,10 +562,7 @@ __webpack_require__.d(exports, {
 /******/ // module cache are used so entry inlining is disabled
 /******/ // startup
 /******/ // Load entry module and return exports
-/******/ var __webpack_exports__ = __webpack_require__(1195);
-/******/ var __webpack_exports__get = __webpack_exports__.get;
-/******/ var __webpack_exports__init = __webpack_exports__.init;
-/******/ export { __webpack_exports__get as get, __webpack_exports__init as init };
+/******/ var __webpack_exports__ = __webpack_require__(4431);
 /******/ 
 
-//# sourceMappingURL=remoteEntry.js.map
+//# sourceMappingURL=main.js.map
---
+++ src_app_microfrontend_module_ts-_06461.js
@@ -0,0 +1,227 @@
+(self["webpackChunkapp"] = self["webpackChunkapp"] || []).push([["src_app_microfrontend_module_ts-_06461"],{
+
+/***/ 1984:
+/*!*************************************************!*\
+  !*** ./src/app/microfrontend-routing.module.ts ***!
+  \*************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "MicroFrontEndRoutingModule": () => (/* binding */ MicroFrontEndRoutingModule)
+/* harmony export */ });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! tslib */ 4929);
+/* harmony import */ var _angular_architects_module_federation__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular-architects/module-federation */ 2162);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ 8230);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_angular_core__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/router */ 8317);
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_angular_router__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+
+const routes = [
+    {
+        path: 'home',
+        loadChildren: () => __webpack_require__.e(/*! import() */ "src_app_home_home_module_ts").then(__webpack_require__.bind(__webpack_require__, /*! ./home/home.module */ 3467)).then(m => m.HomePageModule)
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'angular13',
+        loadChildren: () => (0,_angular_architects_module_federation__WEBPACK_IMPORTED_MODULE_2__.loadRemoteModule)({
+            type: 'module',
+            remoteEntry: 'https://dzurrahman.github.io/mfe-roki-test/remoteEntry.js',
+            exposedModule: './HomeModule',
+        }).then((m) => {
+            return m.HomeModule;
+        }),
+    },
+    {
+        path: 'second',
+        loadChildren: () => __webpack_require__.e(/*! import() */ "src_app_second_second_module_ts").then(__webpack_require__.bind(__webpack_require__, /*! ./second/second.module */ 4714)).then(m => m.SecondPageModule)
+    },
+];
+let MicroFrontEndRoutingModule = class MicroFrontEndRoutingModule {
+};
+MicroFrontEndRoutingModule = (0,tslib__WEBPACK_IMPORTED_MODULE_3__.__decorate)([
+    (0,_angular_core__WEBPACK_IMPORTED_MODULE_0__.NgModule)({
+        imports: [
+            _angular_router__WEBPACK_IMPORTED_MODULE_1__.RouterModule.forChild(routes)
+        ],
+        exports: [_angular_router__WEBPACK_IMPORTED_MODULE_1__.RouterModule]
+    })
+], MicroFrontEndRoutingModule);
+
+
+
+/***/ }),
+
+/***/ 9594:
+/*!*****************************************!*\
+  !*** ./src/app/microfrontend.module.ts ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "MicroFrontEndModule": () => (/* binding */ MicroFrontEndModule)
+/* harmony export */ });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! tslib */ 4929);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ 8230);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_angular_core__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _microfrontend_routing_module__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./microfrontend-routing.module */ 1984);
+
+
+
+let MicroFrontEndModule = class MicroFrontEndModule {
+};
+MicroFrontEndModule = (0,tslib__WEBPACK_IMPORTED_MODULE_2__.__decorate)([
+    (0,_angular_core__WEBPACK_IMPORTED_MODULE_0__.NgModule)({
+        entryComponents: [],
+        imports: [
+            _microfrontend_routing_module__WEBPACK_IMPORTED_MODULE_1__.MicroFrontEndRoutingModule
+        ],
+        providers: [],
+    })
+], MicroFrontEndModule);
+
+
+
+/***/ }),
+
+/***/ 2162:
+/*!********************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/@angular-architects/module-federation/node_modules/@angular-architects/module-federation-runtime/fesm2015/angular-architects-module-federation-runtime.js ***!
+  \********************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "loadRemoteEntry": () => (/* binding */ loadRemoteEntry),
+/* harmony export */   "loadRemoteModule": () => (/* binding */ loadRemoteModule)
+/* harmony export */ });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ 4929);
+
+
+const containerMap = {};
+const remoteMap = {};
+let isDefaultScopeInitialized = false;
+function lookupExposedModule(key, exposedModule) {
+    return (0,tslib__WEBPACK_IMPORTED_MODULE_0__.__awaiter)(this, void 0, void 0, function* () {
+        const container = containerMap[key];
+        const factory = yield container.get(exposedModule);
+        const Module = factory();
+        return Module;
+    });
+}
+function initRemote(container, key) {
+    return (0,tslib__WEBPACK_IMPORTED_MODULE_0__.__awaiter)(this, void 0, void 0, function* () {
+        // const container = window[key] as Container;
+        // Do we still need to initialize the remote?
+        if (remoteMap[key]) {
+            return container;
+        }
+        // Do we still need to initialize the share scope?
+        if (!isDefaultScopeInitialized) {
+            yield __webpack_require__.I('default');
+            isDefaultScopeInitialized = true;
+        }
+        yield container.init(__webpack_require__.S.default);
+        remoteMap[key] = true;
+        return container;
+    });
+}
+function loadRemoteEntry(remoteEntryOrOptions, remoteName) {
+    return (0,tslib__WEBPACK_IMPORTED_MODULE_0__.__awaiter)(this, void 0, void 0, function* () {
+        if (typeof remoteEntryOrOptions === 'string') {
+            const remoteEntry = remoteEntryOrOptions;
+            return yield loadRemoteScriptEntry(remoteEntry, remoteName);
+        }
+        else if (remoteEntryOrOptions.type === 'script') {
+            const options = remoteEntryOrOptions;
+            return yield loadRemoteScriptEntry(options.remoteEntry, options.remoteName);
+        }
+        else if (remoteEntryOrOptions.type === 'module') {
+            const options = remoteEntryOrOptions;
+            yield loadRemoteModuleEntry(options.remoteEntry);
+        }
+    });
+}
+function loadRemoteModuleEntry(remoteEntry) {
+    return (0,tslib__WEBPACK_IMPORTED_MODULE_0__.__awaiter)(this, void 0, void 0, function* () {
+        if (containerMap[remoteEntry]) {
+            return Promise.resolve();
+        }
+        return yield import(/* webpackIgnore:true */ remoteEntry).then(container => {
+            initRemote(container, remoteEntry);
+            containerMap[remoteEntry] = container;
+        });
+    });
+}
+function loadRemoteScriptEntry(remoteEntry, remoteName) {
+    return (0,tslib__WEBPACK_IMPORTED_MODULE_0__.__awaiter)(this, void 0, void 0, function* () {
+        return new Promise((resolve, reject) => {
+            // Is remoteEntry already loaded?
+            if (containerMap[remoteName]) {
+                resolve();
+                return;
+            }
+            const script = document.createElement('script');
+            script.src = remoteEntry;
+            script.onerror = reject;
+            script.onload = () => {
+                const container = window[remoteName];
+                initRemote(container, remoteName);
+                containerMap[remoteName] = container;
+                resolve();
+            };
+            document.body.appendChild(script);
+        });
+    });
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function loadRemoteModule(options) {
+    return (0,tslib__WEBPACK_IMPORTED_MODULE_0__.__awaiter)(this, void 0, void 0, function* () {
+        let loadRemoteEntryOptions;
+        let key;
+        // To support legacy API (< ng 13)
+        if (!options.type) {
+            options.type = 'script';
+        }
+        if (options.type === 'script') {
+            loadRemoteEntryOptions = {
+                type: 'script',
+                remoteEntry: options.remoteEntry,
+                remoteName: options.remoteName
+            };
+            key = options.remoteName;
+        }
+        else if (options.type === 'module') {
+            loadRemoteEntryOptions = {
+                type: 'module',
+                remoteEntry: options.remoteEntry,
+            };
+            key = options.remoteEntry;
+        }
+        if (options.remoteEntry) {
+            yield loadRemoteEntry(loadRemoteEntryOptions);
+        }
+        return yield lookupExposedModule(key, options.exposedModule);
+    });
+}
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+
+
+
+/***/ })
+
+}])
+//# sourceMappingURL=src_app_microfrontend_module_ts-_06461.js.map
